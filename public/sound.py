125499d1248d58a128b955cdca164276842163f1
nvheymann <nico@agentur-pottkinder.de>
2021-11-25 21:07:50 +0100

    Zwischen Stand

---
+++ public/sound.py
@@ -0,0 +1,11 @@
+from playsound import playsound
+import sys
+
+def hello():
+    playsound("/home/nico/Desktop/Raspberry-Soundboard/public/sound/Test.mp3")
+
+
+if __name__ == "__main__":
+    #a = sys.argv[1]
+    #hello(a)
+    hello()
